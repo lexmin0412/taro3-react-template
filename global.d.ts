9d7a4cbf3678789e4388efa6ab142e8580a24785
张乐 <zhangle@hydee.com>
2019-12-23 22:00:30 +0800

    chore: 添加node全局变量

--- global.d.ts
+++ global.d.ts
@@ -12,7 +12,17 @@ declare module "*.styl";
 // @ts-ignore
 declare const process: {
   env: {
-    TARO_ENV: 'weapp' | 'swan' | 'alipay' | 'h5' | 'rn' | 'tt' | 'quickapp' | 'qq';
+		/**
+		 * taro环境变量
+		 */
+		TARO_ENV: 'weapp' | 'swan' | 'alipay' | 'h5' | 'rn' | 'tt' | 'quickapp' | 'qq';
+		/**
+		 * node环境变量
+		 */
+		NODE_ENV: 'development' | 'uat' | 'production';
+		/**
+		 * 扩展其他属性
+		 */
     [key: string]: any;
   }
 }
