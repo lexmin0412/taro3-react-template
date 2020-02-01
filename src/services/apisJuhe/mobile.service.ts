6436674e33d78996a6b8e6d1ed0b42720b7954da
cathe-zhang <zhangle_media@hotmail.com>
2020-02-01 15:49:06 +0800

    feat: 新增手机号码归属地查询service

---
+++ src/services/apisJuhe/mobile.service.ts
@@ -0,0 +1,23 @@
+import BaseRequest from '~/utils/request'
+
+class MobileService extends BaseRequest {
+  constructor() {
+    super({
+      hostKey: 'apisJuhe'
+    })
+	}
+
+	/**
+	 * 查询手机号码归属地
+	 */
+  queryMobile(params: {
+		phoneNumber: number|string
+	}): Promise<any> {
+		const { phoneNumber } = params
+    return this.get({
+      url: `/mobile/get?phone=${phoneNumber}&key=01316ea1229f3e75d538fb346e290e4b`,
+    })
+  }
+}
+
+export default new MobileService() as MobileService
