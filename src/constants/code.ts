370e9c60efa647e8c7bc535cc653aecd94dedf5f
cathe-zhang <zhangle_media@hotmail.com>
2020-03-14 18:40:04 +0800

    refactor: constants模块整合

---
+++ src/constants/code.ts
@@ -0,0 +1,15 @@
+// 接口返回code
+
+// 成功code
+export const SUCC_LIST = [
+  '0',
+	'00000',
+	'10000',
+]
+
+// 登录失效code
+export const LOGIN_FAILURE_LIST = [
+	'99999',
+	'40000',
+	'40001'
+]
---
+++ src/constants/index.ts
@@ -0,0 +1,19 @@
+/**
+ * 导出常量
+ */
+const Constants = {
+  /**
+   * token字段
+   */
+  MASK_TOKEN: 'maskToken',
+  /**
+   * 最后一次登录失效的时间戳
+   */
+  LOGIN_FAILURE_TIMESTAMP: 'loginFailureTimeStamp',
+  /**
+   * 拦截器自定义头部key
+   */
+  INTERCEPTOR_HEADER: 'interceptor-custom-header'
+}
+
+export default Constants
