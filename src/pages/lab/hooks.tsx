9012943382d35664b763be1b07e1281288732082
cathe-zhang <zhangle_media@hotmail.com>
2020-03-15 19:03:07 +0800

    feat: add hooks componnet

---
+++ src/pages/lab/hooks.tsx
@@ -0,0 +1,29 @@
+import { ComponentType } from 'react'
+import Taro, { Component, Config } from '@tarojs/taro'
+import { View, Button, Text } from '@tarojs/components'
+import { observer, inject } from '@tarojs/mobx'
+
+import './hooks.scss'
+
+type PageStateProps = {
+  counterStore: {
+    counter: number,
+    increment: Function,
+    decrement: Function,
+    incrementAsync: Function
+  }
+}
+
+interface Index {
+  props: PageStateProps;
+}
+
+const Index = () => (
+  <View className='index'>
+    <Button onClick={this.increment}>设置</Button>
+    <Button onClick={this.increment}>嘿嘿</Button>
+  </View>
+)
+
+
+export default Index  as ComponentType
