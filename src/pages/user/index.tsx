5cc119b372d5fb299cb28d9cd9cf66a5b662e05f
cathe-zhang <zhangle_media@hotmail.com>
2020-03-15 19:03:42 +0800

    feat: add user page

---
+++ src/pages/user/index.tsx
@@ -0,0 +1,90 @@
+import { ComponentType } from 'react'
+import Taro, { Component, Config } from '@tarojs/taro'
+import { View, Button, Text } from '@tarojs/components'
+import { observer, inject } from '@tarojs/mobx'
+
+import './indes.scss'
+
+type PageStateProps = {
+  counterStore: {
+    counter: number,
+    increment: Function,
+    decrement: Function,
+    incrementAsync: Function
+  }
+}
+
+interface Index {
+  props: PageStateProps;
+}
+
+@inject('counterStore')
+@observer
+class Index extends Component {
+
+  /**
+   * 指定config的类型声明为: Taro.Config
+   *
+   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
+   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
+   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
+   */
+  config: Config = {
+    navigationBarTitleText: '我的'
+  }
+
+  componentWillMount () {
+    console.log('this.props', this.props)
+  }
+
+  componentWillReact () {
+    console.log('componentWillReact')
+  }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  increment = () => {
+    const { counterStore } = this.props
+    counterStore.increment()
+  }
+
+  decrement = () => {
+    const { counterStore } = this.props
+    counterStore.decrement()
+  }
+
+  incrementAsync = () => {
+    const { counterStore } = this.props
+    counterStore.incrementAsync()
+  }
+
+  /**
+   * 按钮组点击
+   */
+  handleBtnClick(type: 'setting') {
+    if ( type === 'setting' ) {
+      Taro.openSetting({
+        success: function(res) {
+          console.log('res', res.authSetting)
+        }
+      })
+    }
+  }
+
+  render () {
+    const { counterStore: { counter } } = this.props
+    return (
+      <View className='index'>
+        <Button onClick={this.handleBtnClick.bind(this, 'setting')}>设置</Button>
+      </View>
+    )
+  }
+}
+
+export default Index  as ComponentType
