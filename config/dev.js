70fca9c728f44dc0051e402d4af4784259c369be
cathe-zhang <zhangle_media@hotmail.com>
2020-03-16 23:17:15 +0800

    feat: 开发服务器添加本地代理

--- config/dev.js
+++ config/dev.js
@@ -4,11 +4,26 @@ module.exports = {
   },
   defineConstants: {
     APP_CONF: {
-      API_HOST: '"https://xx.com/"',
+      API_HOST: '"/api"',
       APPID: '"wx0b32dc740be4b1f5"',
       APIS_JUHE: '"https://wldoperation.58wld.com"',
       API_MAP_QQ: '"https://apis.map.qq.com"',
       KEY_MAP_QQ: '"UQPBZ-RCU36-K2YS3-EMV6Y-JI6JJ-3WBUM"',
+    }
+  },
+  h5: {
+    devServer: {
+      port: '9000',
+      proxy: {
+        '/api': {
+          target: 'https://middle.test.ydjia.cn/businesses-gateway/customer',
+          changeOrigin: true,
+          ws: false,
+          pathRewrite: {
+            '^/api': ``
+          }
+        },
+      },
     }
   }
 }
