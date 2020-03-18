714959b9a6327eabb9daa72533d27e51016aa00c
cathe-zhang <zhangle_media@hotmail.com>
2020-03-18 22:42:04 +0800

    refactor: 分离buildPlugin中的环境变量检查，独立文件

--- build/plugins/buildPlugin.js
+++ build/plugins/buildPlugin.js
@@ -1,27 +1,16 @@
+const checkEnv = require('./checkEnv')
+const getComponent = require('./getComponent')
+
 class BuildPlugin {
   apply (builder) {
     builder.hooks.beforeBuild.tap('BuildPlugin', (config) => {
       // 在这里做一些打包之前的验证操作 可以获取到配置且可以修改
 
-      // 验证环境变量是否完整
-      console.log(`开始检查环境变量，环境标识：${config.env.NODE_ENV}`)
-      const APP_CONF = config.defineConstants.APP_CONF
-      const APP_CONF_ATTRS = [
-        'API_HOST',
-        'APPID',
-        'APIS_JUHE',
-        'API_MAP_QQ',
-        'KEY_MAP_QQ'
-      ]
-      APP_CONF_ATTRS.forEach((item,index)=>{
-        // 如果不存在则报错
-        if ( !APP_CONF[item] ) {
-          throw `缺少环境变量: ${item}`
-        } else {
-          console.log(`${item}: ${APP_CONF[item]}`)
-        }
-      })
-      console.log('环境变量检查通过')
+      // 获取所有组件生成文件名
+      getComponent()
+
+      // 检查环境变量
+      checkEnv(config)
     })
 
     builder.hooks.afterBuild.tap('BuildPlugin', (stats) => {
---
+++ build/plugins/checkEnv.js
@@ -0,0 +1,27 @@
+/**
+ * 检查环境变量
+ * @param config 配置对象
+ */
+const checkEnv = (config) => {
+  // 验证环境变量是否完整
+  console.log(`开始检查环境变量，环境标识：${config.env.NODE_ENV}`)
+  const APP_CONF = config.defineConstants.APP_CONF
+  const APP_CONF_ATTRS = [
+    'API_HOST',
+    'APPID',
+    'APIS_JUHE',
+    'API_MAP_QQ',
+    'KEY_MAP_QQ'
+  ]
+  APP_CONF_ATTRS.forEach((item,index)=>{
+    // 如果不存在则报错
+    if ( !APP_CONF[item] ) {
+      throw `缺少环境变量: ${item}`
+    } else {
+      console.log(`${item}: ${APP_CONF[item]}`)
+    }
+  })
+  console.log('环境变量检查通过')
+}
+
+module.exports = checkEnv
