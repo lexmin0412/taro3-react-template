e68b1862089098eb60cf757271eee88ee5157df6
cathe-zhang <zhangle_media@hotmail.com>
2020-04-01 09:56:53 +0800

    build: 环境变量配置

--- config/pro.js
+++ config/pro.js
@@ -5,8 +5,9 @@ module.exports = {
   defineConstants: {
     APP_CONF: {
       API_HOST: '"https://xx.com/"',
-      APPID: '"wx0b32dc740be4b1f5"',
+      APPID: '"wx60f68c675fb06120"',
       API_MAP_QQ: '"https://apis.map.qq.com"',
+      KEY_MAP_QQ: '"UQPBZ-RCU36-K2YS3-EMV6Y-JI6JJ-3WBUM"',
     }
   },
   mini: {},
--- config/sit.js
+++ config/sit.js
@@ -5,7 +5,9 @@ module.exports = {
   defineConstants: {
     APP_CONF: {
       API_HOST: '"https://xx.com/"',
-      APPID: '"wx0b32dc740be4b1f5"',
+      APPID: '"wx7e6890d20639bee0"',
+      API_MAP_QQ: '"https://apis.map.qq.com"',
+      KEY_MAP_QQ: '"UQPBZ-RCU36-K2YS3-EMV6Y-JI6JJ-3WBUM"',
     }
   },
 }
