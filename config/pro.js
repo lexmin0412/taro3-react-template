cab39c6d7954b902e6708f94f2e376863ef295e8
cathe-zhang <zhangle_media@hotmail.com>
2020-04-02 01:51:05 +0800

    build: 替换appid

--- config/pro.js
+++ config/pro.js
@@ -5,7 +5,7 @@ module.exports = {
   defineConstants: {
     APP_CONF: {
       API_HOST: '"https://xx.com/"',
-      APPID: '"wx60f68c675fb06120"',
+      APPID: '"wx0b32dc740be4b1f5"',
       API_MAP_QQ: '"https://apis.map.qq.com"',
       KEY_MAP_QQ: '"UQPBZ-RCU36-K2YS3-EMV6Y-JI6JJ-3WBUM"',
     }
