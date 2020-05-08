68aedf13421d3884cdabbf30806b0d9747c4636b
cathe-zhang <zhangle_media@hotmail.com>
2020-05-08 14:41:48 +0800

    feat: Obj类新增pickAttrFromObj

--- src/utils/obj.ts
+++ src/utils/obj.ts
@@ -3,11 +3,18 @@
  */
 
 class Obj {
-
-  /**
-   * 从对象中获取属性
-   */
-  pic() {
-
+	/**
+	 * 提取对象属性，返回新对象
+	 * @param {object} obj 提取对象
+	 * @param {Array} propArr 键值数组
+	 */
+  pickAttrFromObj = (obj: any, propArr: Array<any>) => {
+    const newObj: any = {}
+    propArr.forEach((item) => {
+      newObj[item] = obj[item]
+    })
+    return newObj
   }
 }
+
+export default new Obj()
