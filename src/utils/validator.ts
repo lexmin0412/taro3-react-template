18cf5d1941f2fd08055272d55c76573ae6219274
cathe-zhang <zhangle_media@hotmail.com>
2020-05-08 14:42:20 +0800

    feat: validator类新增rules属性

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -5,55 +5,86 @@ import IDCard from '~/utils/idcard'
  * 表单验证类
  */
 class FormValidator {
-  constructor() { }
-
-  /**
-   * 验证函数列表
-   */
+	/**
+	 * 验证函数列表 可直接使用
+	 */
   funcs = {
-    /**
-     * 非空验证
-     */
-    NOT_EMPTY: val => val,
-    /**
-     * 手机号验证
-     */
-    MOBILE: (value: any) => /^1[23456789]\d{9}$/.test(value),
-    /**
-     * 身份证合法验证
-     */
-    IDCARD: value => IDCard.getIDCardInfo(value).isValid
+		/**
+		 * 非空验证
+		 */
+    _notEmpty: (val) => val,
+		/**
+		 * 手机号验证
+		 */
+    _isMobile: (value: any) => /^1[23456789]\d{9}$/.test(value),
+		/**
+		 * 身份证合法验证
+		 */
+    _isIDCard: (value) => IDCard.getIDCardInfo(value).isValid,
   }
 
-  /**
-   * 表单验证方法
-   * @param rules 验证规则数组
-   * @param showToast 是否弹出错误信息
-   */
-  validate(rules: {
-    [key: string]: Array<{
-      /**
-       * 验证规则
-       */
-      test: Function;
-      /**
-       * 错误信息
-       */
-      errMsg: string;
-    }>
-  }, showToast: boolean, obj: any): {
-    /**
-     * 是否验证通过
-     */
-    success: boolean;
-    /**
-     * 错误信息
-     */
-    errMsg: string;
-    /**
-     * 获取到的表单数据对象 验证通过时返回
-     */
-    formData?: any;
+	/**
+	 * 预置的规则 可直接使用
+	 * 示例：
+	 ```
+	validateRules = {
+			phoneNumber: [
+					Validator.rules._isMobile
+			],
+	}
+	 ```
+	 */
+  rules = {
+		/**
+		 * 手机号验证规则
+		 */
+    _isMobile: {
+      test: this.funcs._isMobile,
+      errMsg: '请输入正确的手机号',
+    },
+		/**
+		 * 身份证号验证规则
+		 */
+    _isIDCard: {
+      test: this.funcs._isIDCard,
+      errMsg: '请输入正确的身份证号',
+    },
+  }
+
+	/**
+	 * 表单验证方法
+	 * @param rules 验证规则数组
+	 * @param showToast 是否弹出错误信息
+	 * @param obj 属性集的父级对象
+	 */
+  validate(
+    rules: {
+      [key: string]: Array<{
+				/**
+				 * 验证规则
+				 */
+        test: Function
+				/**
+				 * 错误信息
+				 */
+        errMsg: string
+      }>
+    },
+    showToast: boolean,
+    obj: any
+  ): {
+		/**
+		 * 是否验证通过
+		 */
+    success: boolean
+		/**
+		 * 错误信息
+		 */
+    errMsg: string
+		/**
+		 * 获取到的表单数据对象 验证通过时返回
+		 */
+    formData?: any
   } {
     console.log('进入验证方法', rules)
 
@@ -61,23 +92,23 @@ class FormValidator {
       console.error('请传入需要进行表单验证的对象')
       return {
         success: false,
-        errMsg: `参数缺失，obj：${obj}`
+        errMsg: `参数缺失，obj：${obj}`,
       }
     }
 
-    let returnObj = {
+    const returnObj = {
       success: true,
       errMsg: 'ok',
-      formData: {}
+      formData: {},
     }
     for (const key in rules) {
       if (rules.hasOwnProperty(key)) {
-        const element = rules[key];
+        const element = rules[key]
 
         console.log('each item', element)
 
         let tempErrMsg = ''
-        element.forEach((item,index)=>{
+        element.forEach((item) => {
           if (item.test(obj[key])) {
             console.log('test success')
             returnObj.formData[key] = obj[key]
@@ -87,13 +118,13 @@ class FormValidator {
               Toast.error(item.errMsg)
             }
             tempErrMsg = item.errMsg
-            throw new Error(item.errMsg);
+            throw new Error(item.errMsg)
           }
         })
-        if ( tempErrMsg ) {
+        if (tempErrMsg) {
           return {
             success: false,
-            errMsg: tempErrMsg
+            errMsg: tempErrMsg,
           }
         }
       }
