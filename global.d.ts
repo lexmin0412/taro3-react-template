4fcc2c5bc7291125b893829c45c5dd72a3346b5b
cathe-zhang <zhangle_media@hotmail.com>
2020-06-10 00:22:10 +0800

    feat: ts全局类型声明扩充

--- global.d.ts
+++ global.d.ts
@@ -10,15 +10,55 @@ declare module "*.sass";
 declare module "*.styl";
 
 declare namespace JSX {
-    interface IntrinsicElements {
-        'import': React.DetailedHTMLProps<React.EmbedHTMLAttributes<HTMLEmbedElement>, HTMLEmbedElement>
-    }
+	interface IntrinsicElements {
+		'import': React.DetailedHTMLProps<React.EmbedHTMLAttributes<HTMLEmbedElement>, HTMLEmbedElement>
+	}
 }
 
 // @ts-ignore
 declare const process: {
-  env: {
-    TARO_ENV: 'weapp' | 'swan' | 'alipay' | 'h5' | 'rn' | 'tt' | 'quickapp' | 'qq';
-    [key: string]: any;
-  }
+	env: {
+		TARO_ENV: 'weapp' | 'swan' | 'alipay' | 'h5' | 'rn' | 'tt' | 'quickapp' | 'qq';
+		/**
+		 * local-本地 dev-开发 sit-测试 uat-预发 pro-生产
+		 */
+		NODE_ENV: 'local' | 'dev' | 'sit' | 'uat' | 'pro'
+		[key: string]: any;
+	}
 }
+
+/**
+ * 环境变量配置
+ */
+declare const APP_CONF: {
+	/**
+	 * 接口HOST
+	 */
+	API_HOST: string
+	/**
+	 * 小程序 appid
+	 */
+	APPID: string
+	/**
+	 * 腾讯地图接口服务域名
+	 */
+	API_MAP_QQ: string
+	/**
+	 * 腾讯地图服务key
+	 */
+	KEY_MAP_QQ: string
+	/**
+	 * 当前项目域名
+	 */
+	APP_HOST: string
+}
+
+/**
+ * 微信jssdk对象
+ */
+declare const wx: any
+
+/**
+ * 微信jsbridge
+ */
+declare const WeixinJSBridge: any
