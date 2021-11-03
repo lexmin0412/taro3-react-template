ca0ad2b0e8847045a0baaf7119ec141b4c381c9c
lexmin0412 <zhangle_media@hotmail.com>
2021-11-04 00:27:21 +0800

    chore: 注释插件

--- config/plugins/index.js
+++ config/plugins/index.js
@@ -1,34 +1,34 @@
 const plugins = [
-	[
-		'@tarox/plugin-init-app',
-		{
-			// 配置首页路由
-			homeRoute: 'pages/index/index',
-			compSuffix: ['.tsx'], // 指定扫描为页面/组件的文件后缀，未在此列的文件将会被过滤
-			subPackages: {
-				// 不纳入分包的文件夹
-				excludes: [
-					'assets',
-					'components',
-					'constants',
-					'enums',
-					'interceptors',
-					'lib',
-					'pages',
-					'services',
-					'store',
-					'styles',
-					'utils',
-					'tencent-webim',
-				],
-			},
-			weapp: {
-				pages: {
-					// 指定不需要打包的页面
-					excludes: [],
-				},
-			},
-		},
-	],
+	// [
+	// 	'@tarox/plugin-init-app',
+	// 	{
+	// 		// 配置首页路由
+	// 		homeRoute: 'pages/index/index',
+	// 		compSuffix: ['.tsx'], // 指定扫描为页面/组件的文件后缀，未在此列的文件将会被过滤
+	// 		subPackages: {
+	// 			// 不纳入分包的文件夹
+	// 			excludes: [
+	// 				'assets',
+	// 				'components',
+	// 				'constants',
+	// 				'enums',
+	// 				'interceptors',
+	// 				'lib',
+	// 				'pages',
+	// 				'services',
+	// 				'store',
+	// 				'styles',
+	// 				'utils',
+	// 				'tencent-webim',
+	// 			],
+	// 		},
+	// 		weapp: {
+	// 			pages: {
+	// 				// 指定不需要打包的页面
+	// 				excludes: [],
+	// 			},
+	// 		},
+	// 	},
+	// ],
 ]
 module.exports = plugins
