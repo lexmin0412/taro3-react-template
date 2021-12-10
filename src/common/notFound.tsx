94966efaa17abf3bf78396042a8a584998697326
lexmin0412 <zhangle_media@hotmail.com>
2021-12-10 16:17:17 +0800

    refactor: notFound组件改为函数组件

--- src/common/notFound.tsx
+++ src/common/notFound.tsx
@@ -2,48 +2,17 @@
  * 404页面
  */
 
-import React, { Component } from 'react'
+import React from 'react'
 import { View } from '@tarojs/components'
-import { observer, inject } from 'mobx-react'
 
 import './notFound.scss'
 
-/**
- * 页面 props
- */
-type PageProps = {
-	app: any
-}
-
-/**
- * 页面 state
- */
-type PageState = {}
-
-interface NotFound {
-	props: PageProps
-	state: PageState
-}
-
-@inject(({ store }) => ({
-	app: store.app,
-}))
-@observer
-class NotFound extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {}
-	}
-
-	componentDidShow() {}
-
-	render() {
-		return (
-			<View className='common-notFound-page'>
-				<View>404页面</View>
-			</View>
-		)
-	}
+const NotFound = (): JSX.Element => {
+	return (
+		<View className='common-notFound-page'>
+			<View>404页面</View>
+		</View>
+	)
 }
 
 export default NotFound
