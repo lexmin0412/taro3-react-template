7cf12ab60c8faa244f55e30929109ad9c50d9fe2
lexmin0412 <zhangle_media@hotmail.com>
2022-01-21 16:12:39 +0800

    feat: 增加页面切换动画配置

--- src/app.config.ts
+++ src/app.config.ts
@@ -34,4 +34,9 @@ export default defineAppConfig({
 		navigationBarTitleText: 'WeChat',
 		navigationBarTextStyle: 'black',
 	},
+	// 页面切换动画
+	animation: {
+		duration: 196, // 动画切换时间，单位毫秒
+		delay: 50, // 切换延迟时间，单位毫秒
+	},
 })
