65e8129bd88c7743e00d31f16a438bbcaf265f19
lexmin0412 <zhangle_dev@outlook.com>
2022-03-22 02:31:45 +0800

    feat: navigation_header 组件 h5 端兼容

--- src/_resources/components/navigation_header/index.tsx
+++ src/_resources/components/navigation_header/index.tsx
@@ -1,12 +1,10 @@
+import Taro from '@tarojs/taro'
 import React from 'react'
-import {
-	View,
-	// Image
-} from '@tarojs/components'
-import { useNavigationBarInfo } from '@/utils/hooks'
-// import icon_back from '@/assets/icons/back.svg'
-// import icon_home from '@/assets/icons/home.svg'
-// import Router from '@/utils/route'
+import { View, Image } from '@tarojs/components'
+import { useNavigationBarInfo, usePageInfo } from '@/utils/hooks'
+import icon_back from '@/assets/icons/back.svg'
+import icon_home from '@/assets/icons/home.svg'
+import Router from '@/utils/route'
 import './index.scss'
 
 type NavigationHeaderProps = {
@@ -22,18 +20,30 @@ export default function NavigationHeader(
 		statusBarHeight,
 		navigationBarHeight,
 		navigationContentHeight,
-		// menuButtonHeight,
+		menuButtonHeight,
 		navigationPaddding,
-		// menuButtonWidth,
+		menuButtonWidth,
 	} = useNavigationBarInfo()
 
-	// const handleBack = () => {
-	// 	Router.navigateBack()
-	// }
+	const { stackLength, isTabbar } = usePageInfo()
+	console.log('pageInfo', stackLength, isTabbar)
 
-	// const handleBackToHome = () => {
-	// 	Router.backToHome()
-	// }
+	const handleBack = () => {
+		Router.navigateBack()
+	}
+
+	const handleBackToHome = () => {
+		Router.backToHome()
+	}
+
+	const iconBoxStyle =
+		process.env.TARO_ENV === 'weapp'
+			? {
+					width: menuButtonWidth,
+			  }
+			: {
+					width: Taro.pxTransform(200),
+			  }
 
 	return (
 		<View
@@ -55,39 +65,37 @@ export default function NavigationHeader(
 					height: `${navigationContentHeight}px`,
 				}}
 			>
-				{/* <View
-					className='nav-header-content-left'
-					style={{
-						width: menuButtonWidth,
-					}}
-				>
-					<View
-						className='nav-header-content-left-icon-group'
-						style={{
-							height: `${menuButtonHeight}px`,
-							borderRadius: `${menuButtonHeight / 2}px`,
-						}}
-					>
-						<Image
-							src={icon_back}
-							className='nav-header-content-left-icon'
-							onClick={handleBack}
-						/>
-						<View className='nav-header-content-left-icon-divider' />
-						<Image
-							src={icon_home}
-							className='nav-header-content-left-icon'
-							onClick={handleBackToHome}
-						/>
-					</View>
-				</View> */}
+				<View className='nav-header-content-left' style={iconBoxStyle}>
+					{!isTabbar ? (
+						<>
+							<View
+								className='nav-header-content-left-icon-group'
+								style={
+									process.env.TARO_ENV === 'weapp'
+										? {
+												height: `${menuButtonHeight}px`,
+												borderRadius: `${menuButtonHeight / 2}px`,
+										  }
+										: {}
+								}
+							>
+								<Image
+									src={icon_back}
+									className='nav-header-content-left-icon'
+									onClick={handleBack}
+								/>
+								<View className='nav-header-content-left-icon-divider' />
+								<Image
+									src={icon_home}
+									className='nav-header-content-left-icon'
+									onClick={handleBackToHome}
+								/>
+							</View>
+						</>
+					) : null}
+				</View>
 				<View className='nav-header-content-center'>{title || '页面标题'}</View>
-				{/* <View
-					className='nav-header-content-right'
-					style={{
-						width: menuButtonWidth,
-					}}
-				/> */}
+				<View className='nav-header-content-right' style={iconBoxStyle} />
 			</View>
 		</View>
 	)
